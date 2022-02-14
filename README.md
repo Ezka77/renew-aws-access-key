f4148423715012078d7b5e7bfacf4b05a02eb1e1
Ezka77 <ezka77@hotmail.com>
2022-02-14 13:49:48 +0100

    Systemd timer

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # AWS renew IAM access key
 
 ## Installation
+
+Clone and install
+
 ```
 git clone git@github.com:Ezka77/renew-aws-access-key.git
 cd renew-aws-access-key
 pip3 install --user -e .
 ```
+
+To update use `git pull`
+
 
 ## Usage
 
@@ -14,3 +20,21 @@ In a terminal:
 $ aws_renew_access_key
 ```
 
+## Crontab ... almost
+
+### Systemd
+
+Copy files from `service` to `~/.config/systemd/user/` and enable the timer:
+
+```
+systemctl --user daemon-reload
+
+# Optional: renew your key
+systemctl --user start aws_key_renew.service
+
+# add a timer: the renew is done monthly
+systemctl --user enable aws_key_renew.timer
+
+# check the timer
+systemctl --user list-timers
+```
--- setup.py
+++ setup.py
@@ -3,32 +3,28 @@ import os
 
 README = """AWS/Boto3 script to re-new IAM access key"""
 
-requires = [
-        'click',
-        'colorama',
-        'boto3',
-        'wheel'
-        ]
+requires = ["click", "colorama", "boto3", "wheel"]
 
-setup(name='AWS access key renew',
-      version='0.1',
-      description='AWS access key renew',
-      long_description=README,
-      author='Ezka77',
-      author_email='',
-      url='',
-      classifiers=[
+setup(
+    name="AWS access key renew",
+    version="0.1",
+    description="AWS access key renew",
+    long_description=README,
+    author="Ezka77",
+    author_email="",
+    url="",
+    classifiers=[
         "Programming Language :: Python",
         "Private :: Do Not Upload",
-        ],
-      keywords='aws renew access key useless',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=requires,
-      entry_points="""\
+    ],
+    keywords="aws renew access key useless",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    entry_points="""\
       [console_scripts]
       aws_renew_access_key = aws_renew_access_key:main
       """,
-      )
+)
